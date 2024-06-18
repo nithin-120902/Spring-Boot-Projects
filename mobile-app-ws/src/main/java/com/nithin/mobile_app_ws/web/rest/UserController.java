90f091897648f4d552478af0fa33b2dcffb05a7e
Nithin Krishna Venna <NithinKrishna.Venna.Contractor@pepsico.com>
2024-06-18 15:52:39 +0530

    Added rest controller.

---
+++ mobile-app-ws/src/main/java/com/nithin/mobile_app_ws/web/rest/UserController.java
@@ -0,0 +1,9 @@
+package com.nithin.mobile_app_ws.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController {
+}
