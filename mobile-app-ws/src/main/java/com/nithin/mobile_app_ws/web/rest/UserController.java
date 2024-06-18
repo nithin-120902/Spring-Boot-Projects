bfa7ce870bc20d037063a13f5f52a4f311d94142
Nithin Krishna Venna <NithinKrishna.Venna.Contractor@pepsico.com>
2024-06-18 15:59:05 +0530

    Adding basic method to handle http requests.

--- mobile-app-ws/src/main/java/com/nithin/mobile_app_ws/web/rest/UserController.java
+++ mobile-app-ws/src/main/java/com/nithin/mobile_app_ws/web/rest/UserController.java
@@ -1,9 +1,28 @@
 package com.nithin.mobile_app_ws.web.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
+
+    @GetMapping
+    public String getuser(){
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createuser(){
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateuser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteuser(){
+        return "delete user was called";
+    }
 }
