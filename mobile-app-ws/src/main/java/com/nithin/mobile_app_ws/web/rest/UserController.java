ecc85992121b79fe2f5fe74871f70a32cc18e133
Nithin Krishna Venna <NithinKrishna.Venna.Contractor@pepsico.com>
2024-06-19 11:58:57 +0530

    Reading Query String Request Parameters.

--- mobile-app-ws/src/main/java/com/nithin/mobile_app_ws/web/rest/UserController.java
+++ mobile-app-ws/src/main/java/com/nithin/mobile_app_ws/web/rest/UserController.java
@@ -6,23 +6,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 
+    @GetMapping()
+    public String getUsers(@RequestParam(value="page") int page,
+                           @RequestParam(value="limit") int limit){
+        return "get users was called with page = " + page + " and limit = " + limit;
+    }
+
     @GetMapping("/{userId}")
-    public String getuser(@PathVariable String userId){
+    public String getUser(@PathVariable String userId){
         return "get user was called by Id="+userId;
     }
 
     @PostMapping
-    public String createuser(){
+    public String createUser(){
         return "create user was called";
     }
 
     @PutMapping
-    public String updateuser(){
+    public String updateUser(){
         return "update user was called";
     }
 
     @DeleteMapping
-    public String deleteuser(){
+    public String deleteUser(){
         return "delete user was called";
     }
 }
